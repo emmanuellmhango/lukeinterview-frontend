94f1e05a482991c6a009e0deb128dbb0e0e164f3
emmanuel mhango <emmanuellmhango@gmail.com>
2023-10-31 14:32:58 +0200

    corrected url for facility registering

--- src/state/url.js
+++ src/state/url.js
@@ -1,5 +1,5 @@
 export const USER_URL = "http://127.0.0.1/api/v1/users";
-export const FACILITY_URL = "https://api-web-service-g48n.onrender.com/api/v1";
+export const FACILITY_URL = "http://127.0.0.1/api/v1/facilities";
 export const MASTER_FACILITY_URL =
   "https://zipatala.health.gov.mw/api/facilities";
 export const MASTER_DISTRICT_URL =
